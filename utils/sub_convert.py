e8eab30c2fb93f7870b02e1885e149df966edf4f
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-01-22 13:25:04 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -314,11 +314,11 @@ class sub_convert():
 
                 proxy_index = proxies_list.index(proxy)
                 if len(proxies_list) > 999:
-                    proxy['name'] = f'{country_code}-{proxy_index:0>4d}'
+                    proxy['name'] = 'aa'
                 elif len(proxies_list) < 999 and len(proxies_list) > 99:
-                    proxy['name'] = f'{country_code}-{proxy_index:0>3d}'
+                    proxy['name'] = 'aa'
                 elif len(proxies_list) < 99:
-                    proxy['name'] = f'{country_code}-{proxy_index:0>2d}'
+                    proxy['name'] = 'aa'
 
                 if proxy['server'] != '127.0.0.1':
                     proxy_str = str(proxy)
