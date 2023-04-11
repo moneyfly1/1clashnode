7c94e4d8471504a59c91b8a41f02cd9c6f4ccfed
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-04-11 17:24:21 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -42,7 +42,7 @@ class sub_convert():
             base64_final
     """
 
-    def convert(raw_input, input_type='url', output_type='url', custom_set={'dup_rm_enabled': True, 'format_name_enabled': False}): # {'input_type': ['url', 'content'],'output_type': ['url', 'YAML', 'Base64']}
+    def convert(raw_input, input_type='url', output_type='url', custom_set={'dup_rm_enabled': True, 'format_name_enabled': True}): # {'input_type': ['url', 'content'],'output_type': ['url', 'YAML', 'Base64']}
         # convert Url to YAML or Base64
         if input_type == 'url': # 获取 URL 订阅链接内容
             sub_content = ''
@@ -232,7 +232,7 @@ class sub_convert():
                         pass
 
             return sub_content_yaml # 返回字典, output 值为 True 时返回修饰过的 YAML 文本
-    def makeup(input, dup_rm_enabled=True, format_name_enabled=True): # 对节点进行区域的筛选和重命名，输出 YAML 文本 
+    def makeup(input, dup_rm_enabled=True, format_name_enabled=False): # 对节点进行区域的筛选和重命名，输出 YAML 文本 
         # 区域判断(Clash YAML): https://blog.csdn.net/CSDN_duomaomao/article/details/89712826 (ip-api)
         if isinstance(input, dict):
             sub_content = input
