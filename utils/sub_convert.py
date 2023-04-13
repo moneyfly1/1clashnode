d3d60ebfbe69ce62c52f6ecc01e2f5c0d11b48c8
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-04-13 19:45:17 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -380,7 +380,8 @@ class sub_convert():
 
                 proxy_index = proxies_list.index(proxy)
                 proxyname=proxy['name']
-                
+                if idid == '24'：
+                    idid = ''
                 if len(proxies_list) >=1000:
                     proxy['name'] = f'{idid}-{proxyname}-{country_code}-{proxy_index:0>4d}'
                 elif len(proxies_list) <= 999 and len(proxies_list) > 99:
