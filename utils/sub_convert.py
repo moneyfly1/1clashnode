1a78fd447de10b73f8a58f2e60825d3fcbb4954f
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-09-25 16:29:16 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -655,7 +655,7 @@ class sub_convert():
             for index in range(len(proxies_list)): # 不同节点订阅链接内容 https://github.com/hoochanlon/fq-book/blob/master/docs/append/srvurl.md
                 proxy = proxies_list[index]
                 
-                if proxy['type'] == 'vmess'  :# Vmess 节点提取, 由 Vmess 所有参数 dump JSON 后 base64 得来。
+                if proxy['type'] == 'vmess' and 'ws-opts' in proxy : # Vmess 节点提取, 由 Vmess 所有参数 dump JSON 后 base64 得来。
 
                     yaml_default_config = {
                         'name': 'Vmess Node', 'server': '0.0.0.0', 'port': 0, 'uuid': '', 'alterId': 0,
