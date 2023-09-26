f7416e59b93bafafcb48b4ca8c9cc5668381fde8
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-09-26 12:14:18 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -707,12 +707,15 @@ class sub_convert():
                         ss_proxy = str('ss://' + ss_base64 + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
                         
                     elif proxy['plugin'] == 'obfs':
-                        ssplugin=str('obfs='+proxy['plugin-opts'][mode]  +   ';'  +  'obfs-host='   +    proxy['plugin-opts'][host])
+                        ssplugin=str('obfs='+proxy['plugin-opts'][mode] + ';' + 'obfs-host=' + proxy['plugin-opts'][host])
                         ssplugin=str(urllib.parse.quote(ssplugin))
                         ss_base64_decoded = str(proxy['cipher']) + ':' + str(proxy['password']) + '@' + str(proxy['server']) + ':' + str(proxy['port'])
                         ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
                         ss_proxy = str('ss://' + ss_base64 +  '/?plugin=obfs-local%3B'+ ssplugin + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
-                        
+                    elif proxy['plugin'] == 'v2ray-plugin':
+                        ss_base64_decoded = str(proxy['cipher']) + ':' + str(proxy['password']) + '@' + str(proxy['server']) + ':' + str(proxy['port'])
+                        ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
+                        ss_proxy = str('ss://' + ss_base64 + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')                        
                     protocol_url.append(ss_proxy)
    
 
