c6e4ccc02185421ea3361fc4839a276ac6677c90
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-09-26 15:52:16 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -539,8 +539,8 @@ class sub_convert():
                     yaml_url.setdefault('cipher', method_part)
                     yaml_url.setdefault('password', password_part)
                     #plugin_list=server_part_list[1].replace('?', '') #plugin=v2ray-plugin%3Bmode%3Dwebs
-                    #print(plugin_list)
-                    plugin_list=urllib.parse.unquote(plugin_list)
+                    print(server_part_list[1])
+                    plugin_list=urllib.parse.unquote(server_part_list[1])
                     print(plugin_list)
 
 
