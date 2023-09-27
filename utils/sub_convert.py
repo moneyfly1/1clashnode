d205dce2adcff52c8e2c982e147efc1f4cf94664
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-09-27 10:53:45 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -582,7 +582,7 @@ class sub_convert():
                     #yaml_url.setdefault('plugin-opts',{'mode':plugin_mode, 'host':plugin_host})
                     plugindata=str("'obfs','plugin-opts':{'mode':'" +plugin_mode + "', 'host':'"+plugin_host+"'}")
                     print(plugindata)
-                    yaml_url.setdefault('plugin',plugindata)
+                    yaml_url.setdefault('Plugin',plugindata)
                     print(yaml_url)
                     
 
