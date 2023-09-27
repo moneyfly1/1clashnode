81f836d49caa5caf8d6a6a4abd1f66097019348c
imyaoxp <100553299+imyaoxp@users.noreply.github.com>
2023-09-27 12:03:13 +0800

    Update sub_convert.py

--- utils/sub_convert.py
+++ utils/sub_convert.py
@@ -718,6 +718,7 @@ class sub_convert():
                         ss_base64_decoded = str(proxy['cipher']) + ':' + str(proxy['password']) + '@' + str(proxy['server']) + ':' + str(proxy['port'])
                         ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
                         ss_proxy = str('ss://' + ss_base64 +  '/?plugin=obfs-local%3B'+ ssplugin + '#' + str(urllib.parse.quote(proxy['name'])) + '\n')
+                        print(ss_proxy)
                     elif proxy['plugin'] == 'v2ray-plugin':
                         ss_base64_decoded = str(proxy['cipher']) + ':' + str(proxy['password']) + '@' + str(proxy['server']) + ':' + str(proxy['port'])
                         ss_base64 = sub_convert.base64_encode(ss_base64_decoded)
